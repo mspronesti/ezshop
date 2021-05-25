be185e124d13dd3b8e4643710d594ef3670b29df
Massimiliano Pronesti <s287646@studenti.polito.it>
2021-05-26 01:15:10 +0200

    feat: add product tests

--- src/test/java/it/polito/ezshop/data/EZShopControllerImplTest.java
+++ src/test/java/it/polito/ezshop/data/EZShopControllerImplTest.java
@@ -2,27 +2,21 @@ package it.polito.ezshop.data;
 
 import static org.junit.Assert.*;
 
-
-
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
-//import org.junit.FixMethodOrder;
 import org.junit.Test;
-//import org.junit.runners.MethodSorters;
 
 import it.polito.ezshop.exceptions.*;
 
 public class EZShopControllerImplTest {
     private static EZShopController controller;
-    private static Integer idCashier, idShopManager, idAdministrator;
+    private static UserRepository userRepository;
+    private static ProductTypeRepository productTypeRepository;
     
     @BeforeClass
-    static public void init() throws InvalidUsernameException, InvalidPasswordException, InvalidRoleException {
+    static public void init() {
     	controller =  EZShopControllerFactory.create();
-    
-		idCashier = controller.createUser("cashguy", "1234", "Cashier");
-		idShopManager = controller.createUser("managerguy", "1234", "ShopManager");
-		idAdministrator = controller.createUser("administratorguy", "1234", "Administrator");
+    	userRepository = new UserRepository();
+    	productTypeRepository = new ProductTypeRepository();
     
     }
 
@@ -53,11 +47,15 @@ public class EZShopControllerImplTest {
 		
 	@Test
 	public void testLoginLogout() throws InvalidUsernameException, InvalidPasswordException {
-		String username = "administratorguy";
+		String username = "Marco";
 		String password = "1234";
 		
+		// empty username or empty password
 		assertThrows(InvalidUsernameException.class, () -> controller.login("", password));
 		assertThrows(InvalidPasswordException.class, () -> controller.login(username, ""));
+		
+		// wrong password
+		assertNull(controller.login(username, "12345"));
 		
 		User user = controller.login(username, password);
 		assertNotNull(user);
@@ -66,12 +64,15 @@ public class EZShopControllerImplTest {
 	
 	@Test
 	public void testDeleteUser() throws InvalidUsernameException, InvalidPasswordException, InvalidUserIdException, UnauthorizedException {
-		User logged = controller.login("cashguy", "1234");
+		User logged = controller.login("Franco", "1234");
+		Integer id1 = userRepository.findByUsername("Marco").getId();
+		Integer id2 = userRepository.findByUsername("Giovanna").getId();
 	    
-		assertThrows(UnauthorizedException.class, () -> controller.deleteUser(1));
+		// unauth attempt to delete user
+		assertThrows(UnauthorizedException.class, () -> controller.deleteUser(id1));
 		controller.logout();
 		
-		logged = controller.login("administratorguy", "1234");
+		logged = controller.login("Marco", "1234");
 		// id null, negative or 0
 		assertThrows(InvalidUserIdException.class, () -> controller.deleteUser(null));
 		assertThrows(InvalidUserIdException.class, () -> controller.deleteUser(-1));
@@ -80,49 +81,53 @@ public class EZShopControllerImplTest {
 		// user doesn't exist
 		assertFalse(controller.deleteUser(10));
 		// correct delete
-		assertTrue(controller.deleteUser(idCashier));
+		assertTrue(controller.deleteUser(id2));
 		controller.logout();	
 	}
 	
 	@Test
-	public void testGetAllUsers() throws InvalidUsernameException, InvalidPasswordException {
-		User loggedUser = controller.login("cashguy", "1234");
+	public void testGetAllUsers() throws InvalidUsernameException, InvalidPasswordException, UnauthorizedException {
+		User loggedUser = controller.login("Franco", "1234");
 		assertThrows(UnauthorizedException.class, () -> controller.getAllUsers());
 		controller.logout();
-		loggedUser = controller.login("administratorguy", "1234");
-		// TODO: to be completed
 		
+		loggedUser = controller.login("Marco", "1234");
+		assertTrue(controller.getAllUsers().stream().allMatch(p -> p instanceof User));
 	}
 	
 	@Test
 	public void testGetUser() throws InvalidUsernameException, InvalidPasswordException, InvalidUserIdException, UnauthorizedException {
-		User loggedUser = controller.login("cashguy", "1234");
-		assertThrows(UnauthorizedException.class, () -> controller.getUser(idShopManager));
+		User logged = controller.login("Franco", "1234");
+		Integer id = userRepository.findByUsername("Anna").getId();
+		
+		assertThrows(UnauthorizedException.class, () -> controller.getUser(id));
 		controller.logout();
-		loggedUser = controller.login("administratorguy", "1234");
+		
+		logged = controller.login("Marco", "1234");
 		
 		// wrong id (negative, 0, null)
 		assertThrows(InvalidUserIdException.class, () -> controller.getUser(-1));
 		assertThrows(InvalidUserIdException.class, () -> controller.getUser(0));
 		assertThrows(InvalidUserIdException.class, () -> controller.getUser(null));
 		
-		// unexisting id
+		// inexistent id
 		assertNull(controller.getUser(10));
 		// correct id
-		assertNotNull(controller.getUser(idShopManager));
+		assertNotNull(controller.getUser(id));
 	}
-	
+
 	@Test 
 	public void testUpdateUserRight() throws InvalidUsernameException, InvalidPasswordException, InvalidUserIdException, InvalidRoleException, UnauthorizedException {
 		String newRole = "Administrator";
+		Integer id = userRepository.findByUsername("Anna").getId();
 		
 		// attempt with unauth user
-		User loggedUser = controller.login("cashguy", "1234");
-		assertThrows(UnauthorizedException.class, () -> controller.updateUserRights(idShopManager, newRole));
+		User loggedUser = controller.login("Franco", "1234");
+		assertThrows(UnauthorizedException.class, () -> controller.updateUserRights(id, newRole));
 		controller.logout();
 		
 		// attempt with auth user
-		loggedUser = controller.login("administratorguy", "1234");
+		loggedUser = controller.login("Marco", "1234");
 		
 		// invalid id
 		assertThrows(InvalidUserIdException.class, () -> controller.updateUserRights(null, newRole));
@@ -130,17 +135,140 @@ public class EZShopControllerImplTest {
 		assertThrows(InvalidUserIdException.class, () -> controller.updateUserRights(0, newRole));
 		
 		// invalid role
-		assertThrows(InvalidRoleException.class,  () -> controller.updateUserRights(idShopManager, "fakeRole"));
+		assertThrows(InvalidRoleException.class,  () -> controller.updateUserRights(id, "fakeRole"));
 		
+		// inexistent user
 		assertFalse(controller.updateUserRights( 10, newRole));
-		assertTrue(controller.updateUserRights(idShopManager, newRole));
+		
+		assertTrue(controller.updateUserRights(id, newRole));
 	}
 	
-
+	@Test 
+	public void testCreateProductType() throws InvalidUsernameException, InvalidPasswordException, InvalidProductDescriptionException, InvalidProductCodeException, InvalidPricePerUnitException, UnauthorizedException {
+		String description = "pasta";
+		String productCode = "012345678967";
+		double pricePerUnit = 1.23;
+		String note = "";
+		
+		// unauth 
+		User loggedUser = controller.login("Franco", "1234");
+		assertThrows(UnauthorizedException.class, () -> controller.createProductType(description, productCode, pricePerUnit, note));
+		
+		// auth
+		loggedUser = controller.login("Marco", "1234");
+				
+		// empty/null description
+		assertThrows(InvalidProductDescriptionException.class, () -> controller.createProductType("", productCode, pricePerUnit, note));
+		assertThrows(InvalidProductDescriptionException.class, () -> controller.createProductType(null, productCode, pricePerUnit, note));
+		
+		// empty/null/notGTin barcode
+		assertThrows(InvalidProductCodeException.class, () -> controller.createProductType(description, "", pricePerUnit, note));
+		assertThrows(InvalidProductCodeException.class, () -> controller.createProductType(description, "null", pricePerUnit, note));
+		assertThrows(InvalidProductCodeException.class, () -> controller.createProductType(description, "012345678901", pricePerUnit, note));
+		
+		// negative or 0 price
+		assertThrows(InvalidPricePerUnitException.class, () -> controller.createProductType(description, productCode, -5, note));
+		assertThrows(InvalidPricePerUnitException.class, () -> controller.createProductType(description, productCode, 0, note));
+		
+		
+		// correct product creating
+		Integer id = controller.createProductType(description, productCode, pricePerUnit, note);
+		assert(id != -1);
+		// attempt to duplicate
+		assert(controller.createProductType(description, productCode, pricePerUnit, note) == -1);
+		controller.logout();
+	}
 	
-	@AfterClass
-	static public void clear() {
-	   controller.reset();
+	@Test
+	public void testUpdateProduct() throws InvalidUsernameException, InvalidPasswordException, InvalidProductIdException, InvalidProductDescriptionException, InvalidProductCodeException, InvalidPricePerUnitException, UnauthorizedException {
+		String newDescription = "pizza";
+		String newCode = "012345678998";
+		double newPrice = 1.15;
+		String newNote = "foo";
+		
+		Integer id = productTypeRepository.findByBarcode("012345678905").getId();
+		
+		// unauth 
+		User loggedUser = controller.login("Franco", "1234");
+		assertThrows(UnauthorizedException.class, () -> controller.updateProduct(id, newDescription, newCode, newPrice, newNote));
+				
+		loggedUser = controller.login("Marco", "1234");
+		
+		// invalid id (null, negative, 0)
+		assertThrows(InvalidProductIdException.class, () -> controller.updateProduct(null, newDescription, newCode, newPrice, newNote));
+		assertThrows(InvalidProductIdException.class, () -> controller.updateProduct(-1, newDescription, newCode, newPrice, newNote));
+		assertThrows(InvalidProductIdException.class, () -> controller.updateProduct(0, newDescription, newCode, newPrice, newNote));
+		
+		// invalid description (null, empty)
+		assertThrows(InvalidProductDescriptionException.class, () -> controller.updateProduct(id, "", newCode, newPrice, newNote));
+//		assertThrows(InvalidProductDescriptionException.class, () -> controller.updateProduct(id, "null", newCode, newPrice, newNote));
+		
+		// invalid barcode (null, empty, invalid gtin)
+		assertThrows(InvalidProductCodeException.class, () -> controller.updateProduct(id, newDescription, "", newPrice, newNote));
+		assertThrows(InvalidProductCodeException.class, () -> controller.updateProduct(id, newDescription, "null", newPrice, newNote));
+		assertThrows(InvalidProductCodeException.class, () -> controller.updateProduct(id, newDescription, "012345678990", newPrice, newNote));
+
+		// invalid price (negative)
+		assertThrows(InvalidPricePerUnitException.class, () -> controller.updateProduct(id, newDescription, newCode, -10.5, newNote));
+		
+		// product doesn't exist
+		assertFalse(controller.updateProduct(10, newDescription, newCode, newPrice, newNote));
+		// barcode already assigned
+//		assertFalse(controller.updateProduct(id, newDescription, "012345678912", newPrice, newNote));
+		// correct update
+		assertTrue(controller.updateProduct(id, newDescription, newCode, newPrice, newNote));
+		controller.logout();
+	}
+	
+	@Test
+	public void testDeleteProduct() {
+		
+	}
+	
+	@Test
+	public void testGetAllProducts() throws UnauthorizedException, InvalidUsernameException, InvalidPasswordException, InvalidRoleException, InvalidUserIdException {
+		
+		String[] roles = {"Marco", "Anna", "Franco"}; // Administrator, ShopManager, Cashier
+		for (String role : roles) {
+			controller.login(role, "1234");
+			assertTrue(controller.getAllProductTypes().stream().allMatch(p -> p instanceof ProductType));
+			controller.logout();
+		}
+		
+	}
+	
+	@Test
+	public void testGetProductByBarCode() throws InvalidUsernameException, InvalidPasswordException, InvalidProductCodeException, UnauthorizedException {
+		String barCode = "012345678912";
+		// unauth 
+		User loggedUser = controller.login("Franco", "1234");
+		assertThrows(UnauthorizedException.class, () -> controller.getProductTypeByBarCode(barCode));
+				
+		loggedUser = controller.login("Marco", "1234");
+		// invalid bar code (null, empty, invalid gtin)
+		assertThrows(InvalidProductCodeException.class, () -> controller.getProductTypeByBarCode(""));
+		assertThrows(InvalidProductCodeException.class, () -> controller.getProductTypeByBarCode("null"));
+		assertThrows(InvalidProductCodeException.class, () -> controller.getProductTypeByBarCode("012345678906"));
+		
+		// inexisting product
+		assertNull(controller.getProductTypeByBarCode("012345678943"));
+		assertNotNull(controller.getProductTypeByBarCode(barCode));
+		controller.logout();
+	}
+	
+	@Test
+	public void testgetProductTypesByDescription() {
+		
+	}
+	
+	@Test
+	public void testUpdateQuantity() {
+		
+	}
+	
+	@Test
+	public void testUpdatePosition() {
+		
 	}
 	
 	
